7062da922a995cc3fceef6bc44127f111b7d0eb9
Kevin Wang <kevin@fossa.com>
2025-12-15 14:03:21 -0800

    shadcn

--- packages/cli/src/commands/ui.ts
+++ packages/cli/src/commands/ui.ts
@@ -2,11 +2,7 @@ import { spawn } from "child_process";
 import pc from "picocolors";
 import { findWorkbookRoot } from "../utils.js";
 
-interface UiOptions {
-  registry?: string;
-}
-
-export async function uiCommand(command: string, args: string[], options: UiOptions) {
+export async function uiCommand(command: string, args: string[]) {
   const workbookPath = await findWorkbookRoot();
 
   if (!workbookPath) {
@@ -14,16 +10,8 @@ export async function uiCommand(command: string, args: string[], options: UiOpti
     process.exit(1);
   }
 
-  // Build shadcn args
-  const shadcnArgs = [command];
-
-  // Add registry flag if specified
-  if (options.registry) {
-    shadcnArgs.push("-r", options.registry);
-  }
-
-  // Add remaining args
-  shadcnArgs.push(...args);
+  // Pass everything through to shadcn
+  const shadcnArgs = [command, ...args];
 
   console.log(pc.dim(`Running: bunx shadcn@latest ${shadcnArgs.join(" ")}`));
 
